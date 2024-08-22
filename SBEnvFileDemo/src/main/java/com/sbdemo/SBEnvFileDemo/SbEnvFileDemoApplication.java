605cb8b65253e37d04258b0ee3a0d5d2a2265062
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-08-22 18:48:27 +0530

    initial commit

---
+++ SBEnvFileDemo/src/main/java/com/sbdemo/SBEnvFileDemo/SbEnvFileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sbdemo.SBEnvFileDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEnvFileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEnvFileDemoApplication.class, args);
+	}
+
+}
